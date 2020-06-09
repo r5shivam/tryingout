354f9b48eb1fa343346748d10abc79ddfc9edd30
r5shivam <49517258+r5shivam@users.noreply.github.com>
2020-06-09 13:36:48 +0530

    just

--- Module4/harder/script.js
+++ Module4/harder/script.js
@@ -1,6 +1,5 @@
 (function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  console.log("Hi");
   for (var i=0;i<names.length;i++){
     if (names[i][0]=="J" || names[i][0]=="j"){
       byeSpeaker.speak(names[i]);
